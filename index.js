a429f8622d622d3817085ba7c13d99bbf3f91733
ramiinberg <rami.inberg@gmail.com>
2022-06-04 11:42:54 +0300

    added dice count to character

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-
 const hero = {
   elementId: "hero",
   name: "Wizard",
   avatar: "images/wizard.jpg",
   health: 60,
-  diceScore: 6
+  diceScore: 6,
+  diceCount: 3
 }
 
 const monster = {
@@ -13,21 +13,29 @@ const monster = {
   name: "Orc",
   avatar: "images/orc.jpg",
   health: 10,
-  diceScore: 4
+  diceScore: 4,
+  diceCount: 1
 }
+
 
 renderCharactar(hero)
 renderCharactar(monster)
 
 
-function renderCharactar({ elementId, name, avatar, health, diceScore }) {
+function renderCharactar({ elementId, name, avatar, health, diceScore, diceCount }) {
+  let diceHtml = ""
+
+  for (let dice = 1; dice <= diceCount; dice++) {
+    diceHtml += "<div class='dice'>6</div>"
+  }
+
   document.getElementById(elementId).innerHTML = `
   <div class="character-card">
     <h4 class="name"> ${name} </h4>
     <img class="avatar" src="${avatar}"/>
     <p class="health">health: <b> ${health} </b></p>
     <div class="dice-container">
-      <div class="dice"> ${diceScore} </div>
+      ${diceHtml}
     </div>
   </div>
   `
