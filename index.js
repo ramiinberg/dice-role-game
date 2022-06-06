e4ba2238911500b2d0065e7596773cb3e1ec4a54
ramiinberg <rami.inberg@gmail.com>
2022-06-06 14:05:34 +0300

    created render function and cleaned code

--- index.js
+++ index.js
@@ -25,26 +25,30 @@ function Character(data) {
   }
   
   this.getCharacterHtml = function() {
-    return document.getElementById(this.elementId).innerHTML = `
-    <div class="character-card">
-      <h4 class="name"> ${this.name} </h4>
-      <img class="avatar" src="${this.avatar}"/>
-      <p class="health">health: <b> ${this.health} </b></p>
-      <div class="dice-container">
-        ${this.getDiceHtml(this.diceCount)}
-      </div>
-    </div>
+    return `<div class="character-card">
+              <h4 class="name"> ${this.name} </h4>
+              <img class="avatar" src="${this.avatar}"/>
+              <p class="health">health: <b> ${this.health} </b></p>
+              <div class="dice-container">
+                ${this.getDiceHtml(this.diceCount)}
+              </div>
+            </div>
     `
   }
 }
 
-const wizard = new Character(hero)
-wizard.getCharacterHtml()
-const orc = new Character(monster)
-orc.getCharacterHtml()
+function render() {
+  const wizard = new Character(hero)
+  document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml()
+
+  const orc = new Character(monster)
+  document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml()
+}
 
 function getDiceRollArray(diceCount) {
   return new Array(diceCount).fill(0).map(function() {
     return Math.floor(Math.random() * 6) + 1
   })
 }
+
+render()
