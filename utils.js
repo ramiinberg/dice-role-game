38403be7084d6ef8c257269570e5f6eb5c49ff74
ramiinberg <rami.inberg@gmail.com>
2022-06-07 08:47:14 +0300

    changed export way

--- utils.js
+++ utils.js
@@ -1,5 +1,7 @@
-export function getDiceRollArray(diceCount) {
+function getDiceRollArray(diceCount) {
   return new Array(diceCount).fill(0).map(function() {
     return Math.floor(Math.random() * 6) + 1
   })
 }
+
+export { getDiceRollArray }
