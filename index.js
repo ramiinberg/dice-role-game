11229de343d0e9dc061b114dc43692639da598ad
ramiinberg <rami.inberg@gmail.com>
2022-06-07 09:12:42 +0300

    Attack button changing dice numbers

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ import Character from "./character.js"
 
 document.getElementById("attack-button").addEventListener("click", attack)
 
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
+
 render()
 
-function render() {
-  const wizard = new Character(characterData.hero)
-  document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
 
-  const orc = new Character(characterData.monster)
+
+function render() {
+  document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
   document.getElementById('monster').innerHTML = orc.getCharacterHtml()
 }
 
 function attack() {
-  console.log('Attack button working')
+  render()
 }
