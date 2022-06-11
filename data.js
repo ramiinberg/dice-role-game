2f0b972b8b62cdb4e68399d7c07903909b82ec74
Rami Inberg <rami.inberg@gmail.com>
2022-06-11 19:57:06 +0300

    added new monsters

--- data.js
+++ data.js
@@ -9,8 +9,22 @@ const characterData = {
   monster: {
     name: "Orc",
     avatar: "images/orc.jpg",
-    health: 20,
+    health: 10,
     diceCount: 1,
+    currentDiceScore: []
+  },
+  demon: {
+    name: "Demon",
+    avatar: "images/orc.png",
+    health: 25,
+    diceCount: 2,
+    currentDiceScore: []
+  },
+  goblin: {
+    name: "Goblin",
+    avatar: "images/orc.png",
+    health: 20,
+    diceCount: 3,
     currentDiceScore: []
   }
 }
--- index.js
+++ index.js
@@ -4,11 +4,15 @@ import Character from "./character.js"
 document.getElementById("attack-button").addEventListener("click", attack)
 
 const wizard = new Character(characterData.hero)
+
+let monsterArray = ["orc", "demon", "goblin"]
 const orc = new Character(characterData.monster)
 
 render()
 
-
+const getNewMonster = () => {
+  const nextMonsterData =characterData[monsterArray.shift()]
+}
 
 function render() {
   document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
